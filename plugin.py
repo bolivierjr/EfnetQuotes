9ea997105c79c8e3a0fd7a7f34f440da419ad467
Bruce Olivier <bolivierjr@gmail.com>
2017-12-16 03:04:49 -0600

    Added delete command function, a little bit of refactoring and some comments.

--- plugin.py
+++ plugin.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 #
 # Copyright (c) 2017, Bruce Olivier
 # All rights reserved.
@@ -33,12 +33,12 @@ class EfnetQuotes(callbacks.Plugin):
     full_path = os.path.dirname(os.path.abspath(__file__))
     db_file = '{0}/data/efnetquotes.db'.format(full_path)
 
-    def create_database(self, irc, db_file, table):
+    def create_database(self, irc, table):
         conn = None
 
         try:
             print('Connecting to SQLite3 database...')
-            conn = sqlite3.connect(db_file)
+            conn = sqlite3.connect(self.db_file)
             cursor = conn.cursor()
 
             sql = """CREATE TABLE IF NOT EXISTS {0} (
@@ -67,6 +67,10 @@ class EfnetQuotes(callbacks.Plugin):
         conn = None
 
         try:
+            """
+            Doing a check to see if there is a file or not.
+            If not, create a database.
+            """
             with open(self.db_file) as f:
                 pass
 
@@ -78,7 +82,7 @@ class EfnetQuotes(callbacks.Plugin):
         except IOError as e:
             irc.reply('No database found. Creating new database...')
             print(e)
-            self.create_database(irc, self.db_file, table)
+            self.create_database(irc, table)
 
         except Error as e:
             print(e)
@@ -98,6 +102,9 @@ class EfnetQuotes(callbacks.Plugin):
             timestamp = calendar.timegm(now.utctimetuple())
             table = '{0}quotes'.format(channel[1:])
 
+            """
+            Making sure the command is called in a channel and not private chat
+            """
             if not channel.startswith('#'):
                 print('You must be in a channel to add a quote.')
                 return
@@ -117,7 +124,7 @@ class EfnetQuotes(callbacks.Plugin):
             print(e)
 
             if str(e).startswith('no such table'):
-                self.create_database(irc, self.db_file, table)
+                self.create_database(irc, table)
                 irc.reply('Creating new database table...try again.')
 
         except AttributeError as e:
@@ -146,16 +153,22 @@ class EfnetQuotes(callbacks.Plugin):
             conn = self.connect(irc, table)
             cursor = conn.cursor()
 
+            """
+            Checking to see if there is an argument or no argument sent with
+            the command. Making sure the command is called in a channel and not 
+            private chat Checking to see if there is an argument or no argument
+            sent with the command.
+            """
             if not channel.startswith('#'):
                 irc.reply('You must be in the channel to use this command')
                 return
 
-            if text is not None:
+            elif text is not None:
                 sql = """SELECT id,nick,quote,channel FROM {0} WHERE channel=?
-                            AND quote LIKE ? ORDER BY random()
+                            AND (id LIKE ? OR quote LIKE ?) ORDER BY random()
                             LIMIT 1;""".format(table)
 
-                cursor.execute(sql, (channel, search,))
+                cursor.execute(sql, (channel, text, search,))
             else:
                 sql = """SELECT id,nick,quote,channel FROM {0} WHERE channel=?
                             ORDER BY random() LIMIT 1;""".format(table)
@@ -164,6 +177,10 @@ class EfnetQuotes(callbacks.Plugin):
 
             quote = cursor.fetchone()
 
+            """
+            Checking to see if one of the select statements returned
+            back a query or no matches.
+            """
             if quote is not None:
                 irc.reply('#{0}: {1}'.format(quote[0], quote[2]))
             else:
@@ -187,6 +204,74 @@ class EfnetQuotes(callbacks.Plugin):
                 print('Closing database connection...')
 
     quote = commands.wrap(quote, [commands.optional('text')])
+
+    def delquote(self, irc, msg, args, text):
+        """<quote number>
+        Use this command with to delete a quote with the given quote number.
+        """
+        conn = None
+
+        try:
+            msg = str(msg).split(' ')
+            host = msg[0][1:]
+            nick = host.split('!')[0]
+            channel = msg[2]
+            search = '%{0}%'.format(text)
+            table = '{0}quotes'.format(channel[1:])
+
+            """
+            Making sure the command is called in a channel and not private chat
+            """
+            if not channel.startswith('#'):
+                irc.reply('You must be in the channel to use this command')
+                return
+
+            conn = self.connect(irc, table)
+            cursor = conn.cursor()
+
+            user = """SELECT nick,host FROM {0} WHERE id=?;""".format(table)
+
+            cursor.execute(user, (text,))
+            author = cursor.fetchone()
+
+            """
+            Checking to see if select statement returned the author
+            of the quote
+            """
+            if author is not None:
+                auth_host = author[1].split('!')[1]
+
+            else:
+                cursor.close()
+                irc.reply('No match/quotes')
+                return
+
+            """
+            Checks to see if a user is the author of the quote.
+            If so, then delete the quote.
+            """
+            if nick == author[0] or host.endswith(auth_host):
+                sql = """DELETE FROM {0} WHERE id=?;""".format(table)
+
+                cursor.execute(sql, (text,))
+                quote = cursor.fetchone()
+                conn.commit()
+                cursor.close()
+                irc.reply('Quote #{0} deleted.'.format(text))
+
+            else:
+                irc.reply('You must be the author to delete.')
+                cursor.close()
+
+        except Error as e:
+            print(e)
+
+        finally:
+            if conn is not None:
+                conn.close()
+                print('Closing database connection...')
+
+    delquote = commands.wrap(delquote, ['text'])
 
 
 Class = EfnetQuotes
